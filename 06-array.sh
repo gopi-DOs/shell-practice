0755152d566533c8542c23d41870c4b4a09ca1ab
gopi-DOs <gopiseelam93@gmail.com>
2024-11-07 14:23:09 +0530

    array

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 #index starts from 0, size is 3 
 FRUITS=("APPLE" "KIWI" "ORANGE") #ARRAY
 
-echo "first fruit is: ${FRUITS(0)}"
-echo "second fruit is: ${FRUITS(1)}"
-echo "third fruit is: ${FRUITS(2)}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is: ${FRUITS[1]}"
+echo "third fruit is: ${FRUITS[2]}"
 
-echo "first fruit is: ${FRUITS(@)}"
+echo "third fruit is: ${FRUITS[@]}"

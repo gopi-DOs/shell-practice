38aeef8206c5dab13d6ef3e1b21fedee1ee6f790
gopi-DOs <gopiseelam93@gmail.com>
2024-11-07 13:36:37 +0530

    variales

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine. How are you doing?"
+echo "$Person1:: I am doing good $Person2. What's going on?"
+echo "$Person2:: I started learing shell script $Person1"

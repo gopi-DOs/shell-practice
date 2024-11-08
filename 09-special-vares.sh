e2c500ab3f7d125fca283eccdacecc7da1199efd
gopi-DOs <gopiseelam93@gmail.com>
2024-11-08 13:08:39 +0530

    special varies

---
+++ 09-special-vares.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all variables passed to the script: $@"
+echo "number of variables passed: $#"
+echo "Script name: $0"
+echo "current directory: $PWD"
+echo "home directory: $HOME"
+echo "PID of the script excution: $$"
+echo :PID of last excution: $!"

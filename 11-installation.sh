7a96efe7f17bb3c2e532c1657a37dcfebd041cd4
gopi-DOs <gopiseelam93@gmail.com>
2024-11-08 13:59:29 +0530

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
- echo "userid is:$USERID"
+# echo "userid is:$USERID"
+
+if [ $userid -ne 0 ]
+then
+ echo "please run the script with root privrlages"
+ fi
+
+ dnf install git -y

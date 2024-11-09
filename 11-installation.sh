bf71e34da6cc4c04030e5c285893a9a635d98709
gopi-DOs <gopiseelam93@gmail.com>
2024-11-09 12:25:55 +0530

    installation ; git push origin main
    git add . ;git commit -m installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -27,12 +27,12 @@ else
     echo "Git already install nothig to do"
 fi    
 
-   dnf list installed mysqlll
+   dnf list installed mysql
 
  if [ $? -ne 0 ]
  then
    echo "mysql is not installed going to install it"
-   dnf install mysqlll -y
+   dnf install mysql -y
    if [ $? -ne 0 ]
     then 
     echo "mysql is not installation is not sucess check it"

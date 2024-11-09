0d10cbb32fcf1ac1cfef76459c0a6b17ca1112a1
gopi-DOs <gopiseelam93@gmail.com>
2024-11-09 13:02:39 +0530

    install

---
+++ 12-functions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+# echo "userid is:$USERID"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+      echo "$2 is failed"
+      exit 1
+    else 
+      echo "$2 is sucess"
+fi 
+}
+if [ $USERID -ne 0 ]
+then
+   echo "please run the script with root privrlages"
+    exit 1
+fi
+  dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+
+   echo "Git is not installed, going to install"
+
+    dnf install git -y
+    VALIDATE $? "Insatlling git"
+         
+else
+    echo "Git already install nothig to do"
+fi    
+
+   dnf list installed mysql
+
+ if [ $? -ne 0 ]
+ then
+   echo "mysql is not installed going to install it"
+   dnf install mysql -y
+    VALIDATE $? "Insatlling mysql"
+ else
+   echo "mysql installed nothing to do"
+fi  

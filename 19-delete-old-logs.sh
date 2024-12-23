108e32c8b333d3531e530469125db7d48dad2c68
gopi-DOs <gopiseelam93@gmail.com>
2024-12-23 20:05:11 +0530

    set

---
+++ 19-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source_dir=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ -d $source_dir ]
+then 
+  echo-e  "$source_dir  $G exits $N"
+else 
+  echo "$source_dir  $R dosen't exits $N"
+  exit 1
+fi

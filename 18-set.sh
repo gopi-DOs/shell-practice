9dd1863d064d6990540055634982073fcbebbd66
gopi-DOs <gopiseelam93@gmail.com>
2024-12-23 19:30:19 +0530

    set

--- 18-set.sh
+++ 18-set.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
  
- set -ex #setting the automatic exit, if we ger error
+ set -ex #setting the automatic exit, if we ger error,set -ex for debug
+
+
+ failure(){
+     echo "failed at: $1:$2"
+ }
+
+ trap 'failure "${lineno}" "$bash_command"' ERR
  echo "hello"
  echoooo "hello"
  echo "hello"
